373100f3bd231a5b69a634c76611476d572708b1
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2024-06-21 15:03:02 -0500

    basic functionality in place

--- js/main.js
+++ js/main.js
@@ -5,23 +5,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     centerPadding: '25%',
     slidesToShow: 1,
     slidesToScroll: 1,
-    // focusOnSelect: true,
-    // arrows: false,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                centerPadding: '15%',
-            }
-        },
-        {
-            breakpoint: 560,
-            settings: {
-                centerPadding: '10%',
-            }
-        }
-    ]
-}
+    variableWidth: true,
+    centerPadding: '10%',
+  }
   console.log('DOM fully loaded and parsed');
   $('.carousel').slick(slickOptions);
 });
